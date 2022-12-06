a4ef2ed8d82e30f3bc7885852831a92c91d803bf
storezhang <storezhang@gmail.com>
2022-12-06 09:20:40 +0800

    feat(提供商): 支持使用ServerChan进行推送

--- server_chan.go
+++ server_chan.go
@@ -1,11 +1,44 @@
 package qingniao
 
+import (
+	"context"
+	"fmt"
+	"github.com/go-resty/resty/v2"
+	"github.com/goexl/gox/field"
+	"github.com/goexl/simaqian"
+)
+
+var _ wechatExecutor = (*serverChan)(nil)
+
 type serverChan struct {
-	token string
+	key string
+
+	http   *resty.Client
+	logger simaqian.Logger
 }
 
-func newServerChan(token string) *serverChan {
+func newServerChan(key string, http *resty.Client, logger simaqian.Logger) *serverChan {
 	return &serverChan{
-		token: token,
+		key: key,
+
+		http:   http,
+		logger: logger,
 	}
+}
+
+func (sc *serverChan) send(ctx context.Context, deliver *wechatDeliver) (id string, status Status, err error) {
+	form := map[string]string{
+		"title": deliver.Title,
+		"desp":  deliver.Content,
+	}
+	url := fmt.Sprintf("https://sctapi.ftqq.com/%s.send", sc.key)
+	if hr, pe := sc.http.R().SetContext(ctx).SetFormData(form).Post(url); nil != pe {
+		err = pe
+	} else if hr.IsError() {
+		sc.logger.Warn("ServerChan返回错误", field.New("status.code", hr.StatusCode()))
+	} else {
+		status = StatusDelivered
+	}
+
+	return
 }
